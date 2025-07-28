3ce995aaceb38efa288faa527118cf97af1d08fd
Saliu Musah <root@localhost.localdomain>
2025-07-28 10:49:50 +0000

    Initail release: spanish-cli-translator v1.0.0

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
 import json
 import os
 
-VERSION = "v 1.0.0"
+VERSION = "v1.0.0"
 
 def main():
-    
     print(f"Mini English To Spanish App {VERSION}")
-    
+
+    # Load both word groups
+    def_article = load_json("/definite_article.json")
+    indef_article = load_json("/indefinite_article.json")
+
+    # User input
     word = get_word()
-    lang_path = "/storage/emulated/0/Documents/lang/spanish"
-    def_article = "/definite_article.json"
-    path = lang_path + def_article
-    
-    
-    
-    with open(path, "r") as f:
-        def_article = json.load(f)
-        
+
     if word in def_article:
-        for x in range(len(def_article[word])):
-            data = def_article[word][x]
-            print("Translation: ", data)
+        print("Translations (Definite Article):")
+        for translation in def_article[word]:
+            print(" -", translation)
+    elif word in indef_article:
+        print("Translations (Indefinite Article):")
+        for translation in indef_article[word]:
+            print(" -", translation)
     else:
         print("Word not found.")
-    
-  
+
+def get_path(file):
+    return "/storage/emulated/0/Documents/lang/spanish" + file
+
+def load_json(path):
+    with open(get_path(path), "r") as f:
+        return json.load(f)
+
 def get_word():
-    word = input("Enter the word to translate: ").lower()
-    return word 
-        
-        
+    return input("Enter the word to translate: ").lower()
+
 if __name__ == "__main__":
     main()
